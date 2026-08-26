2c444472fd01f2e930ebd9d22d27da21198536fe
nandstar <63760063+nandstar@users.noreply.github.com>
2026-08-26 22:21:51 +0530

    Add: boats-to-save-people - submission-0

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-0.py
@@ -0,0 +1,17 @@
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        people.sort()
+        left = 0
+        right = len(people) - 1
+        boats = 0
+        
+        while left <= right:
+            # If the lightest and heaviest person can share a boat
+            if people[left] + people[right] <= limit:
+                left += 1
+            # The heaviest person always gets a boat
+            right -= 1
+            boats += 1
+            
+        return boats
+        
